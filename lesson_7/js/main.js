2f9f7b9a6cb0221eefb1b7b19f06a9312ea5fb20
aponomareva <enigmasnape@gmail.com>
2019-10-11 10:49:31 +0300

    lesson_7

---
+++ lesson_7/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let start = document.querySelector('#start');
+
+let budgetValue = document.querySelector('.budget-value');
+let daybudgetValue = document.querySelector('.daybudget-value');
+let levelValue = document.querySelector('.level-value');
+let expensesValue = document.querySelector('.expenses-value');
+let optionalexpensesValue = document.querySelector('.optionalexpenses-value');
+let incomeValue = document.querySelector('.income-value');
+let monthsavingsValue = document.querySelector('.monthsavings-value');
+let yearsavingsValue = document.querySelector('.yearsavings-value');
+
+let expensesItems = document.querySelectorAll('.expenses-item');
+
+let buttons = document.querySelectorAll('button');
+let expensesItemBtn = buttons[0];
+let optionalExpensesBtn = buttons[1];
+
+let optionalExpensesItems = document.querySelectorAll('.optionalexpenses-item');
+
+let chooseIncome = document.querySelector('.choose-income');
+let chooseSum = document.querySelector('.choose-sum');
+let choosePercent = document.querySelector('.choose-percent');
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
